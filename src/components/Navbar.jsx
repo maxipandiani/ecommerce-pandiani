16da058e591de1660e7042343a3d00cbf196f68a
maxipandiani <maximilianop77@hotmail.com>
2021-12-19 20:50:05 -0300

    desafio 2

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Barra = () => {
+  return (
+    <div className='estilo'>
+      <h1>Ordenati</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="/#">Home</a>
+          </li>
+          <li>
+            <a href="/#">Productos</a>
+          </li>
+          <li>
+            <a href="/#">Contacto</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Barra
