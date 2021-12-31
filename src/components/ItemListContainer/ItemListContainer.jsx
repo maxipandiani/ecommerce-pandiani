d7b722f10382f84dd6982f04038c72d7db139a00
maxipandiani <maximilianop77@hotmail.com>
2021-12-30 22:30:37 -0300

    desafio 5

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import Itemlist from '../Itemlist/Itemlist.jsx';
+import {getFetch} from '../Stock/Stock.jsx';
+import './ItemListContainer.css';
 
-function ItemListContainer({greetings}) {
+
+function ItemListContainer ({greetings}) {
+    
+    const [merc, setmerc] = useState([])
+    const [loading, setloading] = useState(true)
+
+    
+    useEffect(() => {
+        getFetch
+        .then(resp => setmerc(resp))
+        .catch(err => console.log(err))
+        .finally(()=> setloading(false))
+    }, [])
+
+
     return (
+
         <div>
-           <a>{greetings}</a> 
+             <a className='saludo' >{greetings}</a>
+
+            {loading ? <h1>cargando...</h1> : <Itemlist mercaderia  = {merc}/>}
+
+            
         </div>
+
     )
 }
+
 
 
 
